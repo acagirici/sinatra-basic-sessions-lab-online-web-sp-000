95797e1a488f84b1f0997a3e870bfdd72658033f
Arif Cagirici <acagirici23@gmail.com>
2021-01-02 04:28:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do 
+    enable :sessions
+    set :session_secret, "id_valid"
+  end
+  
+  get / do
+    erb :index
+  end
+  
+  post '/checkout' do
+    
+    erb :checkout
+  end
+  
 end
